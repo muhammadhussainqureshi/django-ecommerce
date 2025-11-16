8c4f8c2bd022b3cd98cd89f4004415beb5e8a45f
Muhammad Hussain Qureshi <mhussainqureshi242@gmail.com>
2025-11-15 19:00:28 -0800

    Register Product model in admin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from .models import Product
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'created_at')
+admin.site.register(Product)
